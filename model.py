97162161b68e9252097816236088916185358ba2
Binbin Dong <binbin.dong1994@gmail.com>
2021-12-14 23:37:05 +0800

    well... this if the first working version

--- model.py
+++ model.py
@@ -1,24 +1,30 @@
-from keras.layers import BatchNormalization
-from keras.layers import Dense, Activation, Input, Dropout, add
+#from keras.layers import BatchNormalization
+from keras.layers import Dense, Activation, Input, Dropout
 from keras.models import Model
-from keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam
 
 def DLModel(InputShape, h_layers, lr=0.001, drops=None, dropout=True):
+	'''
+	InputShape = number of input variables
+	h_layers = number of nodes in each layer
+	lr = learning rate
+	drops = dropout rate in each hidden layer
+	dropout = if you want to turn on dropout in trianing and testing'''
+	
 	In = Input(shape=[InputShape,])
 	x = In
 	for i, h in enumerate(h_layers[:]):
 		x = Dense(h, kernel_initializer='glorot_uniform')(x)
-		x = BatchNormalization()(x)
+		#x = BatchNormalization()(x)
 		x = Activation('relu')(x)
-		if dropout:
-			x = Dropout(drops[i])(x)
+		x = Dropout(drops[i])(x, training=dropout)
 	predictions = Dense(1, activation='sigmoid')(x)
 
-	model = Model(input=In, outputs=predictions)
+	model = Model(inputs=In, outputs=predictions)
 
 	model_optimizer = Adam(lr=lr)
 
-	model.complile(
+	model.compile(
 		loss = 'binary_crossentropy',
 		optimizer = model_optimizer,
 		metrics=['accuracy'],
--- train.py
+++ train.py
@@ -13,27 +13,45 @@ import lib_plotting
 import argparse
 
 parser = argparse.ArgumentParser(description='Options for launching trianings...')
-######
-# add argument here
-######
+parser.add_argument('-i', '--input', type=str, default="input/bdong_total_shuffled.h5")
+parser.add_argument('-o', '--outputDir', type=str, default="output")
+args = parser.parse_args()
 
 #####
 # Parameter setting here
-InputShape = 8
+InputShape = 10
+h_layers = [20, 10, 3]
+drops = [0.2, 0.2, 0.2]
+dropout=True
+lr = 0.0001
+batch_size = 128
+epochs = 4
+
 doPlotting = True
 
+
 ##### Data loading
+trainfile = h5py.File(args.input, 'r')
+totalEvents = len(trainfile['X_train'])
+halfEvents = int(0.5 * totalEvents)
+X_train = trainfile['X_train'][:halfEvents]
+Y_train = trainfile['labels'][:halfEvents]
+X_test = trainfile['X_train'][halfEvents:]
+Y_test = trainfile['labels'][halfEvents:]
 
 ##### Training model parameter setting and loading
-Model.summary()
+TrainModel = model.DLModel(InputShape=InputShape, h_layers=h_layers, lr=lr, drops=drops, dropout=dropout)
+TrainModel.summary()
 
-callbacks = EarlyStopping(monitor='val_loss', practice=5)
-history = Model.fit(X_train, Y_train,
-			batch_size = args.batch_size,
-			epochs = args.epoch,
+##### Hello!! Adding useful early stop...
+callbacks = EarlyStopping(monitor='val_loss', patience=5)
+history = TrainModel.fit(X_train, Y_train,
+			batch_size = batch_size,
+			epochs = epochs,
 			validation_data = (X_test, Y_test),
 			callbacks = callbacks,
-			verbose = 2)
+			verbose = 1)
+TrainModel.save('{}/training_e{}.h5'.format(args.outputDir,epochs))
 
 
 train_loss = history.history['loss']
@@ -42,5 +60,5 @@ val_loss = history.history['val_loss']
 val_acc = history.history['val_accuracy']
 
 if doPlotting:
-	lib_plotting.plotAccLoss(train_loss, val_loss, output_dir=args.output)
-	lib_plotting.plotAccLoss(train_acc, val_acc, putVar='Acc', output_dir=args.output)
+	lib_plotting.plotAccLoss(train_loss, val_loss, putVar='Loss', output_dir=args.outputDir)
+	lib_plotting.plotAccLoss(train_acc, val_acc, putVar='Acc', output_dir=args.outputDir)
