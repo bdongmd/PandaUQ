0a23db8f9c9b3dc076833ba78ab4969e73fd8d3d
Binbin Dong <binbin.dong1994@gmail.com>
2021-12-15 20:07:09 +0800

    added performance plotting

--- lib_plotting.py
+++ lib_plotting.py
@@ -1,8 +1,89 @@
+from keras.backend import sqrt
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.backends.backend_pdf
 import json
 import pandas as pd
 import sys
+from tqdm import tqdm 
+
+def plotOutputScore(score, labels, output_dir='output'):
+	''' Plot output scores of the NN modeli, and purity stuff '''
+	outputScore = np.array(score)
+	labels = np.array(labels)
+	scanvalue = np.linspace(0., 1.0, num=100)
+	cut = []
+	sig_eff = []
+	purity = []
+	signal = labels[labels==1]
+	signal_score = score[labels==1]
+	significance = []
+	N_signal = []
+
+	for i in tqdm(range(len(scanvalue)), desc="========== Caluting ROC curve..."):
+		signal_tagged = signal[signal_score>scanvalue[i]]
+		all_tagged = labels[score>scanvalue[i]]
+		if len(all_tagged)==0:
+			break
+		cut.append(scanvalue[i])
+		N_signal.append(len(signal_tagged))
+		significance.append(len(signal_tagged)/np.sqrt(len(all_tagged)))
+		sig_eff.append(len(signal_tagged)/len(signal))
+		purity.append(len(signal_tagged)/len(all_tagged))
+	
+	print("========== Plotting testing performance...")
+	pdf = matplotlib.backends.backend_pdf.PdfPages("{}/testing_perm.pdf".format(output_dir))
+	## first is the simple output distribution for signal and background
+	fig,ax = plt.subplots(figsize=(10,10), ncols=1, nrows=1)
+	nbins=200
+	ax.hist(outputScore[labels==1], bins=nbins, range=[0,1], density=True, label = 'signal', histtype='step')
+	ax.hist(outputScore[labels==0], bins=nbins, range=[0,1], density=True, label = 'background', histtype='step')
+	ax.set_ylabel('density')
+	ax.set_xlabel('sigmoid output')
+	#ax.set_yscale('log')
+	ax.legend()
+	pdf.savefig()
+	fig.clear()
+	plt.close(fig)
+
+	## plot significance as a funciton of cut
+	fig,ax = plt.subplots(figsize=(10,10), ncols=1, nrows=1)
+	color = 'tab:red'
+	ax.plot(cut, significance, 'o', color=color)
+	ax.set_ylabel('significance', color=color)
+	ax.set_xlabel('cut')
+	ax.tick_params(axis='y', labelcolor = color)
+
+	ax2 = ax.twinx()
+	color = 'tab:green'
+	ax2.set_ylabel('N. of tagged signal events', color=color)
+	ax2.plot(cut, N_signal, color = color)
+	ax2.tick_params(axis='y', labelcolor = color)
+
+	pdf.savefig()
+	fig.clear()
+	plt.close(fig)
+
+	## plot eff versus purity
+	fig,ax = plt.subplots(figsize=(10,10), ncols=1, nrows=1)
+	ax.plot(cut, sig_eff, 'o', label="efficiency", color = 'blue')
+	ax.plot(cut, purity, 'o', label="purity", color='orange')
+	ax.set_xlabel('cut')
+	ax.set_ylim(0, 1.2)
+	ax.legend()
+	pdf.savefig()
+	fig.clear()
+	plt.close(fig)
+
+	fig,ax = plt.subplots(figsize=(10,10), ncols=1, nrows=1)
+	ax.plot(sig_eff, purity, 'o', color = 'blue')
+	ax.set_xlabel('efficiency')
+	ax.set_ylabel('purity')
+	pdf.savefig()
+	fig.clear()
+	plt.close(fig)
+
+	pdf.close()
 
 def plotAccLoss(trainInput, testInput, putVar, output_dir='plots'):
 	'''comparison between train and testing loss and accuracy'''
