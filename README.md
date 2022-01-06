a970eb8aaa7348534dd1d91f58ffe8a73f92c270
Binbin Dong <binbin.dong1994@gmail.com>
2022-01-06 16:16:55 +0800

    adding more variables as input

--- README.md
+++ README.md
@@ -25,9 +25,10 @@ python3 ToSmallerRoot.py -i ../input/sig_total.root -s S2Only -o ../input/bdong_
 - Convert data format from root to hdf5
 For S2only training, can directly use the ```conver.py``` to get the hdf5. So far, listed 23 variables for training. Command:
 ```
-python3 convert.py -s ../input/sig_total.root -b ../input/bkg_total.root --saveScale -c input_Variables.json -o ../input/total_shuffled.h5
+python3 convert.py -b ../input/bkg_total.root -s ../input/original/rn_wall_30_200_reclu_pe.root --s2 ../input/original/tot_ds_sample_reclu_pe.root --saveScale -c input_Variables.json -o ../input/total_shuffled_3class.h5
 ```
 In order to save scaling info for training variables, use flag ```--saveScale```. Otherwise, the file containing scaling info will be loaded. \\
+(In the above case, 3 classes instead of 2 are used. Label 0: background, 1: signal from wall, 2: signal from ds)
 
 - Training:
 ```
--- SamplePrep/convert.py
+++ SamplePrep/convert.py
@@ -1,5 +1,5 @@
 from json import dump, load
-from numpy.core.fromnumeric import compress, var
+from keras.utils import np_utils
 import uproot3 as up
 import numpy as np
 import h5py
@@ -10,7 +10,6 @@ from pickle import dump, load
 from var_mapping import mapping
 import sys
 
-from uproot3.source.compressed import Compression
 sys.path.append('../')
 import lib_plotting
 
@@ -23,18 +22,20 @@ def get_max(tree, branch, index):
 	for i in range(tree.numentries):
 		max_value.append(var[i][ith[i]])
 	return max_value
-vector_varible = ["qS2Tenth", "qS2FWHM1", "qS2FWHM3", "wS2CDF", "wS2CDF5", "wS2CDF50", "wS2CDF90", "nPMTS2", "nPeakS2", "qS2maxHitCharge", "qS2maxChannelCharge", "qS2hitStdev", "qS2channelStdev"]
+vector_varible = ["qS2Tenth", "qS2FWHM1", "qS2FWHM2","qS2FWHM3", "widthTenS2","wS2CDF", "wS2CDF5", "wS2CDF50", "wS2CDF90", "wS2FWHM", "nPMTS2", "nPeakS2", "ratioqS2PrePeak", "qS2maxHitCharge", "qS2maxChannelCharge", "qS2BmaxHitCharge", "qS2hitStdev", "qS2channelStdev"]
 
 parser = argparse.ArgumentParser(description='Add input and output files.')
-parser.add_argument('-s', '--sigfile', type=str,
-	default='/Users/binbindong/Desktop/work/PandaX/PandaUQ/input/simu_2hit_bdt.root')
 parser.add_argument('-b', '--bkgfile', type=str,
 	default='/Users/binbindong/Desktop/work/PandaX/PandaUQ/input/total_pair_test_2hit.root')
+parser.add_argument('-s', '--sigfile', type=str,
+	default='/Users/binbindong/Desktop/work/PandaX/PandaUQ/input/simu_2hit_bdt.root')
 parser.add_argument('--s2', type=str)
 parser.add_argument('--saveScale', action='store_true')
 parser.add_argument('-c', '--config', type=str)
 parser.add_argument('-o', '--outputfile', type=str)
 args = parser.parse_args()
+
+saveSig2 = True # Save s2 for training if True, else only for comparing the distributions between two distributions but do not save.
 
 signal = up.open(args.sigfile)["event_tree"]
 bkg = up.open(args.bkgfile)["event_tree"]
@@ -53,21 +54,31 @@ print("======= Signal shape: {}".format(df_S.shape))
 print("======= Background shape: {}".format(df_B.shape))
 
 if args.s2:
-	sig2 = up.open(args.s2)['acc_tree']
+	sig2 = up.open(args.s2)['event_tree']
 	df_S2 = sig2.pandas.df(var_list)
 	for i in range(len(vector_varible)):
 		df_S2["{}_max".format(vector_varible[i])] = get_max(sig2, vector_varible[i], index='iS2_max')	
-	lib_plotting.variable_plotting(df_S, df_B, sig2=df_S2, noname=False,variables=args.config, outputFile='plots/inputCompare.pdf')
+	print("======= Second signal shape: {}".format(df_S2.shape))
 
 ######## plotting input variables #########
 ## note: no scaling is applied here
-lib_plotting.variable_plotting(df_S, df_B, noname=False, variables=args.config, outputFile='plots/inputVar_noscale.pdf')
+if args.s2:
+	lib_plotting.variable_plotting(df_S, df_B, sig2=df_S2, noname=False,variables=args.config, outputFile='plots/inputVar_noscale.pdf')
+else:
+	lib_plotting.variable_plotting(df_S, df_B, noname=False, variables=args.config, outputFile='plots/inputVar_noscale.pdf')
 
 sWeight = 10. 
 bWeight = 1.
-X_train = np.concatenate((pd.DataFrame(df_B), pd.DataFrame(df_S)))
-labels = np.concatenate((np.zeros(len(df_B), dtype=int), np.ones(len(df_S), dtype=int)))
-weight = np.concatenate((np.ones(len(df_B))*bWeight, np.ones(len(df_S))*sWeight))
+if saveSig2:
+	X_train = np.concatenate((pd.DataFrame(df_B), pd.DataFrame(df_S), pd.DataFrame(df_S2)))
+	labels = np.concatenate((np.zeros(len(df_B), dtype=int), np.ones(len(df_S), dtype=int), 2*np.ones(len(df_S2), dtype=int)))
+	Y_train = np_utils.to_categorical(labels, 3, dtype=int)
+	s2Weight = 10
+	weight = np.concatenate((np.ones(len(df_B))*bWeight, np.ones(len(df_S))*sWeight, np.ones(len(df_S2)*s2Weight)))
+else:
+	X_train = np.concatenate((pd.DataFrame(df_B), pd.DataFrame(df_S)))
+	labels = np.concatenate((np.zeros(len(df_B), dtype=int), np.ones(len(df_S), dtype=int)))
+	weight = np.concatenate((np.ones(len(df_B))*bWeight, np.ones(len(df_S))*sWeight))
 print("======= X_train shape: {}".format(X_train.shape))
 
 scaler = StandardScaler()
@@ -79,9 +90,9 @@ else:
 	scaler = load(open('VariableScaler.pkl', 'rb'))
 X_train = scaler.transform(X_train)
 if args.s2:
-	lib_plotting.variable_plotting(pd.DataFrame(scaler.transform(df_S)), pd.DataFrame(scaler.transform(df_B)), pd.DataFrame(scaler.transform(df_S2)), noname=True, variables=args.config, outputFile='plots/inputCompare_scaled.pdf')
-
-lib_plotting.variable_plotting(pd.DataFrame(scaler.transform(df_S)), pd.DataFrame(scaler.transform(df_B)), noname=True, variables=args.config, outputFile='plots/inputVar_scaled.pdf')
+	lib_plotting.variable_plotting(pd.DataFrame(scaler.transform(df_S)), pd.DataFrame(scaler.transform(df_B)), pd.DataFrame(scaler.transform(df_S2)), noname=True, variables=args.config, outputFile='plots/inputVar_scaled.pdf')
+else:
+	lib_plotting.variable_plotting(pd.DataFrame(scaler.transform(df_S)), pd.DataFrame(scaler.transform(df_B)), noname=True, variables=args.config, outputFile='plots/inputVar_scaled.pdf')
 
 rng_state = np.random.get_state()
 np.random.shuffle(X_train)
@@ -89,11 +100,16 @@ np.random.set_state(rng_state)
 np.random.shuffle(labels)
 np.random.set_state(rng_state)
 np.random.shuffle(weight)
+if saveSig2:
+	np.random.set_state(rng_state)
+	np.random.shuffle(Y_train)
 #assert X_train.shape[1] == 10 ## number of input variables 
 
 outputfile = h5py.File(args.outputfile, 'w')
 outputfile.create_dataset('X_train', data=X_train, compression='gzip')
 outputfile.create_dataset('labels',  data=labels,  compression='gzip')
 outputfile.create_dataset('weight', data=weight, compression='gzip')
+if saveSig2:
+	outputfile.create_dataset('Y_train', data=Y_train, compression='gzip')
 outputfile.close()
 
